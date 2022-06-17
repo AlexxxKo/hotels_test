d5743635da3eb5d701a2eb62d280dca01e674e5f
AlexKo <help.mail015@gmail.com>
2022-06-17 09:32:33 +0300

    Task 1-3

---
+++ Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать метод/функцию, который/которая на вход принимает массив городов. В качестве результата возвращает строку, где города разделены запятыми, а в конце стоит точка. Например, «Москва, Санкт-Петербург, Воронеж.»
+
+string GetPropositionCity(string[] arr) {
+    string propCity = String.Empty;
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1) {
+            propCity += $"{arr[i]}.";
+        }
+        else {
+            propCity += $"{arr[i]}, ";
+        }
+    }
+    return propCity;
+}
+
+string[] arrayCity = {"City1", "City2", "City3", "City8"};
+string Cities = GetPropositionCity(arrayCity);
+
+Console.WriteLine(Cities);
+
+// Time: ~30 мин.
---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать метод/функцию, который/которая на вход принимает число (float), а на выходе получает число, округленное до пятерок. 
+
+int FiveNumber(double num) {
+    bool isNegative = num < 0 ? true : false;
+
+    int round = (int) Math.Abs(Math.Round(num, 0));
+    int rest = round % 10;
+    
+    if (rest < 5) {
+        round = round - rest;
+        if (rest > 2) round =+ 5;
+    } else {
+        round = round - rest + 5;
+        if (rest > 7) round += 5;
+    }
+
+    return isNegative ? -round : round;
+}
+
+double number = -2.73;
+int lastNum = FiveNumber(number);
+Console.WriteLine(lastNum);
+
+// Time: ~30 мин.
